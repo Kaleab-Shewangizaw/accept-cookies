3ad21bc79d473d49e4babf19f3c263c0fbf686b8
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2025-01-05 02:26:41 +0300

    finished the cookie pop-up

--- Index.js
+++ Index.js
@@ -1,2 +1,41 @@
+const donation = document.getElementById("donation");
+const close = document.getElementById("close-btn");
+const formEl = document.getElementById("donation-form");
+const btns = document.getElementById("btns");
+const declineBtn = document.getElementById("decline-btn");
+const donTxt = document.getElementById("don-text")
 
-const donation = document.getElementById('donation');
+close.disabled = true;
+
+close.addEventListener('click', ()=>{
+  donation.style.display = "none"
+})
+
+setTimeout(() => {
+  donation.style.display = "flex";
+}, 2000);
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(formEl);
+  const name = formData.get("d-name");
+
+  if (name.trim() === "") {
+    alert("Please enter your name.");
+    return;
+  }
+  else{
+  formEl.style.display = "none";
+  donTxt.innerHTML = "Please wait ..."
+  setTimeout(()=>{
+    donTxt.innerHTML = `haha you got hacked 
+    <p class = "u-name">${name}</p>, thank you for the donation of <p class= "u-name">$5000</p>.`
+    close.disabled = false
+  }, 3000)}
+});
+
+//I want to do toogle clss on hover over the declineBtn
+
+declineBtn.addEventListener("mouseover", () => {
+  btns.classList.toggle("reverse");
+});
